14ed55229ef3b3e4f462342afc0376c45a1dca6c
Mel DeJesus <meldejesus@Mels-MBP.netgear.com>
2017-05-17 14:24:26 -0400

    This will make an accurate match

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -1,24 +1,180 @@
+
+
+var data=[];
+var e=[];
+var newSurvey= {};
+var d=0;
+  
   $(document).ready(function() {
+
+ // -------------------------------------- special css
+  	//this enables the dropdown menus
     $('select').material_select();
+  	//this enables the modal
+  
+   $('.modal').modal();
+// -------------------------------------- button pressed, object created and posted. 
 
-var hello;
-var list=[];
-$('button').on("click", function() {
-	console.log("click");
-    for (var i=1; i<6; i++){
-     console.log(hello);
-    hello=$("#"+i).val();
-    console.log(hello);
-    list.push(hello);
-    console.log(list);
+$("button").on("click", function(event) {
+
+ 
+      event.preventDefault();
+
+      // Here we grab the dropdown data
+      for (var i=1; i<5; i++) {
+      	data1=$("#"+i).val();
+      	if (isNaN(parseInt(data1))){
+      		$("#warn").text("You must fill out all the options");
+      		return;
+      	}
+
+      	data.push(parseInt(data1));
+      }
+      // Here, we create the object to post 
+      newSurvey = {
+        name: $("#name1").val().trim(),
+        phoneNumber: $("#email1").val().trim(),
+        data: data 
+      };
+
+      console.log(newSurvey);
+
+      // This posts the "newSurvey" object to "/api/survey" and offers the callback function "data", which will return true or false depending of if the post is successful or not.  
+
+      $.post("/api/survey", newSurvey,
+        function(data) {
+
+          // Success
+          if (data) {
+            alert("Yay! You've been entered!");
+          }
+
+          // Error
+          else {
+            alert("Sorry you are on the wait list");
+          }
+
+          // Clear the form when submitting
+          $("#name1").val("");
+          $("#email1").val("");
+         for (var i=1; i<11; i++) {
+         	$("#i").val("");
+      			};
+
+
+        });// Post Closes
+
+
+
+
+// -------------------------------------  This returns a match to the user.
+
+$.get("/api/survey", function(data) {
+// console.log(data);
+// 	console.log(data[0].data[0]);
+	 
+
+        // ------------------ > Pick out amatch
+
+console.log("main:" + parseInt(data[0].data))
+console.log("new: " +newSurvey.data);
+for (var a=0; a<(data.length-1); a++){ 
+	console.log("group " + a );
+	for (var b=0; b<4; b++){
+		// console.log(parseInt(data[a].data[b]));
+// console.log( parseInt(data[a].data[b]) + " - " + newSurvey.data[b] + " = ");
+		var c=parseInt(data[a].data[b])-(newSurvey.data[b]);
+		// console.log(c);
+		c=Math.abs(c);
+
+			// console.log(c);
+		d=d+c;
+	
+		c=0;
+		// console.log(d);
 }
-$('#here').text(list);
-  });
+
+e.push(d);
+d=0;
+console.log(e);
+}
+
+var f=0;
+var g=0;
+var value = e[0];
+for (var i = 1; i < e.length; i++) {
+  if (e[i] < value) {
+    value = e[i];
+    f = i;
+  }
+  else if (e[i]===value){
+  	g=i
+  }
+}
+ 
+console.log("This is the index: "+ f + " and then " +g);
+
+        				
+ // var f=0;
+ $.get("/api/survey", function(data) {
+          $("#name").text(data[f].name);
+      });
+          // $("#email").text(data[f].email);
+         
+   
+ 
+  });  //RETURNS A MATCH FUNCTION CLOSES
+
+}) /// ON CLICK FUNCTION CLOSES
+
+
+// list = [
+// {name: "john",
+//  email: "smith"}, 
+//  {name: "mary",
+// email: "doe"}
+// ];
+  
+// var hello;
+
+// var data=[];
+// $('button').on("click", function() {
+
+// var list ={};
+// list={
+// 	name: $("#name").val().trim(), 
+// 	email: $("#email").val().trim(),
+// 	data: []
+// };
+//     for (var i=1; i<6; i++){
+//     	hello=$("#"+i).val();
+//     	list.data.push(hello);
+//     	   }
+// console.log(list);
+// $('#here').text(list);
 
 
 
+// $.post("/api/survey", list)
+// .done(function(data) {
+// 	console.log(data);
+// }) //end done function.
 
-});
+
+// var currentURL = window.location.origin;
+
+//         // The AJAX function uses the URL of our API to GET the data associated with it (initially set to localhost)
+//         $.ajax({url: currentURL + "/api/survey", method: "GET"})
+//             .done(function(data) {
+// 	$("#name").text(data[0].name);
+// 	$("#email").text(data[0].email); 
+//  });  
+ 
+
+//   });//end of button function
+
+
+}); /// end of js
 
 
 		
--- server.js
+++ server.js
@@ -1,77 +1,172 @@
 // Dependencies
-// =============================================================
+// ===========================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = 3000;
 
-// Sets up the Express app to handle data parsing
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Waiting List (DATA)
-// =============================================================
-var reservations = [{
-  name: "india",
-  phone: "919-816-6990",
-  email: "india@gmail.com",
-  id: "test"
-}];
 
-var waitingList = [];
+// Data
+// ===========================================================
+var data = [{
+  name: "Yoda",
+  email: "Jedi Master",
+  data: [1, 2, 3, 4]
+}];
+ 
+
+// Create one more data entry for the character Obi Wan Kenobi.
+// Enter any values you like for the parameters following the same format as the Yoda and Darth Maul character
+//
+
+// YOUR CODE GOES HERE
+
+//
 
 // Routes
-// =============================================================
-
-// Basic route that sends the user first to the AJAX Page
-app.use(express.static(path.join(__dirname, 'app/public')));
-
-
-
-app.use("/node_modules", express.static('node_modules'));
-//route to the home page...
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-
-// route to the survey page... 
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
-
-
-// provides the api for the json of surveys
-app.get("/api/survey", function(req, res) {
-
-  return res.json(totalSurveys);
-});
-
-// route to the wating list - provides JSON
-// app.get("/api/waitlist", function(req, res) {
-
-//   return res.json(waitingList);
+// ===========================================================
+// app.get("/", function(req, res) {
+//   res.send("Welcome to the Star Wars Pages!ddddddd");
 // });
 
-// Takes in JSON input of survey
-app.post("app/survey", function(req, res) {
-  var newsurvey = req.body;
-  // newres.routeName = newres.name.replace(/\s+/g, "").toLowerCase();
+//route to the home page...
 
-  console.log(newsurvey);
+var newcharacter;
 
-    surveyTotal.push(newsurvey);
+app.use(express.static(path.join(__dirname, 'app/public')));
 
-  res.json(newsurvey);
+app.use("/node_modules", express.static('node_modules'));
+
+ app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "app/public/home.html"));
+ });
+
+ // route to the survey page... 
+ app.get("/survey", function(req, res) {
+   res.sendFile(path.join(__dirname, "app/public/survey.html"));
+ });
+
+app.get("/api/survey", function(req, res) {
+  res.json(data);
+   
+
 });
 
-// Starts the server to begin listening
-// =============================================================
+app.post("/api/survey", function(req, res) {
+  
+   newcharacter = req.body;
+  data.push(newcharacter);
+ console.log(newcharacter);
+ res.json(data);
+});
+
+ 
+
+// Create a new Express route that leads users to the new Obi Wan Kenobi Data
+// Follow the same format as the Yoda and Darth Maul routes
+//
+
+// YOUR CODE GOES HERE
+//
+//
+
+ 
+// Listener
+// ===========================================================
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
+
+
+// // Dependencies
+// // =============================================================
+// var express = require("express");
+// var bodyParser = require("body-parser");
+// var path = require("path");
+
+// // Sets up the Express App
+// // =============================================================
+// var app = express();
+// var PORT = 3000;
+
+// // Sets up the Express app to handle data parsing
+// // app.use(bodyParser.json());
+// // app.use(bodyParser.urlencoded({ extended: true }));
+// // app.use(bodyParser.text());
+// // app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// // Waiting List (DATA)
+// // =============================================================
+
+// var totalSurveys = {
+//   name: "india",
+//   phone: "919-816-6990",
+//   email: "india@gmail.com",
+//   id: "test"
+// };
+// console.log(totalSurveys);
+ 
+
+// // Routes
+// // =============================================================
+
+// // // Basic route that sends the user first to the AJAX Page
+// // app.use(express.static(path.join(__dirname, 'app/public')));
+
+
+
+// // app.use("/node_modules", express.static('node_modules'));
+// // //route to the home page...
+// // app.get("/", function(req, res) {
+// //   res.sendFile(path.join(__dirname, "app/public/home.html"));
+// // });
+
+// // // route to the survey page... 
+// // app.get("/survey", function(req, res) {
+// //   res.sendFile(path.join(__dirname, "app/public/survey.html"));
+// // });
+
+
+// // provides the api for the json of surveys
+// app.get("api/survey", function(req, res) {
+//   res.send("Welcome");
+//   totalSurveys={name:"name"};
+//   res.json(totalSurveys);
+//   console.log(totalSurveys);
+// });
+
+// // route to the wating list - provides JSON
+// // app.get("/api/waitlist", function(req, res) {
+
+// //   return res.json(waitingList);
+// // });
+
+// // Takes in JSON input of survey
+
+
+// // app.post("app/public/api/survey", function(req, res) {
+// //   var newsurvey = req.body;
+// //   // newres.routeName = newres.name.replace(/\s+/g, "").toLowerCase();
+
+// //   console.log(newsurvey);
+
+// //     totalSurveys.push(newsurvey);
+
+// //   res.json(totalSurveys);
+// //   console.log(totalSurveys);
+// // });
+
+// // Starts the server to begin listening
+// // =============================================================
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT);
+// });
