64de593149f455297c290138a43f40e0e860c440
Mel DeJesus <meldejesus@Mels-MacBook-Pro.local>
2017-05-18 17:46:08 -0400

    i returned the questions to 10, but now that the routing is seperate from the server.js file, the $.get api/survey doesn't work

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,25 +1,25 @@
 exports.data = [{
   name: "Yoda",
   email: "https://img.clipartfest.com/8d3f4eebde8d42f8350d7df1777f239f_star-wars-yoda-coloring-pages-star-wars-background-clipart-yoda_500-500.jpeg",
-  data: [1, 2, 3, 4]
+  data: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
 },
 {
   name: "Nikita",
   email: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR38uIwhWWHpjGo-rsU_YHzW4XdZsYrpVbi0clfOWMsaY5-YhNrGA", 
-  data: [4, 4, 4, 4]
+  data: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
 },
 {
   name: "Leah",
   email: "http://assets2.ignimgs.com/2014/06/05/princess-leia-1280jpg-189929_1280w.jpg",
-  data: [1, 1, 1, 1]
+  data: [1, 5, 1, 5, 1, 5, 1, 5, 1, 5]
 },
 {
   name: "Jimmy",
   email: "https://s3.amazonaws.com/media.headshotcrew.com/styles/large/s3/photos/10/54/17/HEADSHOTS-jimmy-image-commercial-photographer-bristol-headshots-portraits-product21.jpg?itok=LBEMeCeA",
-  data: [1,1,1,1]
+  data: [2, 4, 2, 4, 2, 4, 2, 4, 2, 4]
 },
 {
   name: "Pablo",
   email: "http://2.bp.blogspot.com/-ig5JKnKsstI/U0bBldsnAAI/AAAAAAAAW0U/mdYtWlCSGbU/s1600/Portada+01.jpg",
-  data: []
+  data: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 }];
--- server.js
+++ server.js
@@ -14,9 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static(path.join(__dirname, 'app/public')));
-// app.use(express.static(path.join(__dirname, 'css')));
 app.use("/node_modules", express.static('node_modules'));
-// Data
+ 
 // ===========================================================
 
  var data=require("./app/data/friends.js");
@@ -33,56 +32,7 @@ require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
 
 
-// app.use(express.static(path.join(__dirname, 'app/public')));
-
-// app.use("/node_modules", express.static('node_modules'));
-
-//  app.get("/", function(req, res) {
-//   res.sendFile(path.join(__dirname, "app/public/home.html"));
-//  });
-
-//  // route to the survey page... 
-//  app.get("/survey", function(req, res) {
-//    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-//  });
-
  
-
-// routing api / survey
-
-// app.get("/api/survey", function(req, res) {
-//   res.json(data);
-   
-
-// });
-
-// app.post("/api/survey", function(req, res) {
-  
-//    newcharacter = req.body;
-//   data.push(newcharacter);
-//  console.log(newcharacter);
-//  res.json(data);
-// });
-
-
-// non-useful importing....
-
-// var files=require('./app/routing/htmlRoutes.js');
- 
-// var api=require('./app/routing/apiRoutes.js');
- 
-// files.files();
-// api.api();
- 
-
-// Create a new Express route that leads users to the new Obi Wan Kenobi Data
-// Follow the same format as the Yoda and Darth Maul routes
-//
-
-// YOUR CODE GOES HERE
-//
-//
-
  
 // Listener
 // ===========================================================
@@ -90,87 +40,4 @@ app.listen(PORT, function() {
    
 });
 
-
-// // Dependencies
-// // =============================================================
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var path = require("path");
-
-// // Sets up the Express App
-// // =============================================================
-// var app = express();
-// var PORT = 3000;
-
-// // Sets up the Express app to handle data parsing
-// // app.use(bodyParser.json());
-// // app.use(bodyParser.urlencoded({ extended: true }));
-// // app.use(bodyParser.text());
-// // app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
-// // Waiting List (DATA)
-// // =============================================================
-
-// var totalSurveys = {
-//   name: "india",
-//   phone: "919-816-6990",
-//   email: "india@gmail.com",
-//   id: "test"
-// };
-// console.log(totalSurveys);
  
-
-// // Routes
-// // =============================================================
-
-// // // Basic route that sends the user first to the AJAX Page
-// // app.use(express.static(path.join(__dirname, 'app/public')));
-
-
-
-// // app.use("/node_modules", express.static('node_modules'));
-// // //route to the home page...
-// // app.get("/", function(req, res) {
-// //   res.sendFile(path.join(__dirname, "app/public/home.html"));
-// // });
-
-// // // route to the survey page... 
-// // app.get("/survey", function(req, res) {
-// //   res.sendFile(path.join(__dirname, "app/public/survey.html"));
-// // });
-
-
-// // provides the api for the json of surveys
-// app.get("api/survey", function(req, res) {
-//   res.send("Welcome");
-//   totalSurveys={name:"name"};
-//   res.json(totalSurveys);
-//   console.log(totalSurveys);
-// });
-
-// // route to the wating list - provides JSON
-// // app.get("/api/waitlist", function(req, res) {
-
-// //   return res.json(waitingList);
-// // });
-
-// // Takes in JSON input of survey
-
-
-// // app.post("app/public/api/survey", function(req, res) {
-// //   var newsurvey = req.body;
-// //   // newres.routeName = newres.name.replace(/\s+/g, "").toLowerCase();
-
-// //   console.log(newsurvey);
-
-// //     totalSurveys.push(newsurvey);
-
-// //   res.json(totalSurveys);
-// //   console.log(totalSurveys);
-// // });
-
-// // Starts the server to begin listening
-// // =============================================================
-// app.listen(PORT, function() {
-//   console.log("App listening on PORT " + PORT);
-// });
